c9268f1d29169e8fe5173c8ee39b9f01f5ad804b
Jbachtiger <jbachtiger@gmail.com>
2022-01-25 11:00:00 +0000

    Add js code to the how to play modal box

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// Modal - Credit Florin Pop Youtube video guide. Walkthrough helped create modal and code was used and modified for project.
+const open = document.getElementById('open');
+const modal_container = document.getElementById('modal_container');
+const close = document.getElementById('close');
+
+open.addEventListener('click', () => {
+    modal_container.classList.add('show')
+});
+
+close.addEventListener('click', () => {
+    modal_container.classList.remove('show')
+});
