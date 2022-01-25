24edfc61e37b85d17926106705cfb645949f5b7b
Jbachtiger <jbachtiger@gmail.com>
2022-01-25 15:35:48 +0000

    Add initial set of quiz questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,7 +21,7 @@ document.getElementById("contact-btn").onclick = function() {
 //Declared constant variables
 const startButton = document.getElementById("start-btn");
 const howToPlayButton = document.getElementById("open");
-const contactUsButton = document.getElementById ('contact-btn');
+const contactUsButton = document.getElementById('contact-btn');
 const nextButton = document.getElementById('next-btn');
 const homeButton = document.getElementById('home-btn');
 const quizQuestions = document.getElementById('quiz-questions');
@@ -31,27 +31,137 @@ const quizQuestions = document.getElementById('quiz-questions');
 startButton.addEventListener('click', startGame);
 
 // Starts game
-function startGame () {
+function startGame() {
     startButton.classList.add('hide');
     howToPlayButton.classList.add('hide');
     contactUsButton.classList.add('hide');
     homeButton.classList.remove('hide');
     nextButton.classList.remove('hide');
     quizQuestions.classList.remove('hide');
+    setNextQuestion();
 }
 
-function nextQuestion () {
-
-}
-
-function selectAnswer () {
+function nextQuestion() {
 
 }
 
-function incrementScore () {
+function selectAnswer() {
 
 }
 
-function incrementIncorrectAnswer () {
+function incrementScore() {
 
 }
+
+function incrementIncorrectAnswer() {
+
+}
+
+const questions = [{
+        question: 'How many planets do we have in our solar system?',
+        answers: [{
+                text: '8',
+                correct: true
+            },
+            {
+                text: '6',
+                correct: false
+            }, {
+                text: '3',
+                correct: false
+            }, {
+                text: '10',
+                correct: false
+            }
+        ]
+    }, {
+        question: 'The closest planet to the Earth is?',
+        answers: [{
+                text: 'Venus',
+                correct: true
+            },
+            {
+                text: 'Pluto',
+                correct: false
+            }, {
+                text: 'Mars',
+                correct: false
+            }, {
+                text: 'Jupitor',
+                correct: false
+            }
+        ]
+    }, {
+        question: 'Which planet is the hottest?',
+        answers: [{
+                text: 'Saturn',
+                correct: false
+            },
+            {
+                text: 'Earth',
+                correct: false
+            }, {
+                text: 'Venus',
+                correct: true
+            }, {
+                text: 'Mercury',
+                correct: false
+            }
+        ]
+    }, {
+        question: 'Which planet is the coldest?',
+        answers: [{
+                text: 'Uranus',
+                correct: false
+            },
+            {
+                text: 'Earth',
+                correct: false
+            }, {
+                text: 'Mars',
+                correct: false
+            }, {
+                text: 'Neptune',
+                correct: true
+            }
+        ]
+    }
+
+    , {
+        question: 'Which planet is famous for having big rings?',
+        answers: [{
+                text: 'Earth',
+                correct: false
+            },
+            {
+                text: 'Saturn',
+                correct: false
+            }, {
+                text: 'Mercury',
+                correct: false
+            }, {
+                text: 'Jupitur',
+                correct: true
+            }
+        ]
+    }, {
+        question: 'What planet is known as the red planet',
+        answers: [{
+                text: 'Mars',
+                correct: true
+            },
+            {
+                text: 'Venus',
+                correct: false
+            }, {
+                text: 'Uranus',
+                correct: false
+            }, {
+                text: 'Neptune',
+                correct: false
+            }
+        ]
+    }
+
+
+]
