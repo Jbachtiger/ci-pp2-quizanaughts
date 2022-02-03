d05c7489479b9e25b69af327ee8d2ae296376c9b
Jbachtiger <jbachtiger@gmail.com>
2022-02-03 12:08:58 +0000

    Remove question.js file and add quiz questions to the main.js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,241 @@
+// List of quiz questions
+const questions = [{
+        question: 'How many planets do we have in our solar system?',
+        answers: [{
+                text: '8',
+                correct: true
+            },
+            {
+                text: '6',
+                correct: false
+            }, {
+                text: '3',
+                correct: false
+            }, {
+                text: '10',
+                correct: false
+            }
+        ]
+    }, {
+        question: 'The closest planet to the Earth is?',
+        answers: [{
+                text: 'Venus',
+                correct: true
+            },
+            {
+                text: 'Pluto',
+                correct: false
+            }, {
+                text: 'Mars',
+                correct: false
+            }, {
+                text: 'Jupiter',
+                correct: false
+            }
+        ]
+    }, {
+        question: 'Which planet is the hottest?',
+        answers: [{
+                text: 'Saturn',
+                correct: false
+            },
+            {
+                text: 'Earth',
+                correct: false
+            }, {
+                text: 'Venus',
+                correct: true
+            }, {
+                text: 'Mercury',
+                correct: false
+            }
+        ]
+    }, {
+        question: 'Which planet is the coldest?',
+        answers: [{
+                text: 'Uranus',
+                correct: false
+            },
+            {
+                text: 'Earth',
+                correct: false
+            }, {
+                text: 'Mars',
+                correct: false
+            }, {
+                text: 'Neptune',
+                correct: true
+            }
+        ]
+    }
+
+    , {
+        question: 'Which planet is famous for having big rings?',
+        answers: [{
+                text: 'Earth',
+                correct: false
+            },
+            {
+                text: 'Saturn',
+                correct: false
+            }, {
+                text: 'Mercury',
+                correct: false
+            }, {
+                text: 'Jupiter',
+                correct: true
+            }
+        ]
+    }, {
+        question: 'What planet is known as the red planet?',
+        answers: [{
+                text: 'Mars',
+                correct: true
+            },
+            {
+                text: 'Venus',
+                correct: false
+            }, {
+                text: 'Uranus',
+                correct: false
+            }, {
+                text: 'Neptune',
+                correct: false
+            }
+        ]
+    },
+    {
+        question: 'Saturn has 150 moons and moonlets',
+        answers: [{
+                text: 'True',
+                correct: true
+            },
+            {
+                text: 'False',
+                correct: false
+            }
+        ]
+    },
+    {
+        question: 'Who was the first person to walk on the moon?',
+        answers: [{
+                text: 'Neil Armstrong ',
+                correct: true
+            },
+            {
+                text: 'Elon Musk',
+                correct: false
+            }, {
+                text: 'William Shatner',
+                correct: false
+            }, {
+                text: 'Tim Peake',
+                correct: false
+            }
+        ]
+    },
+    {
+        question: "What is the name of NASA's most famous space telescope?",
+        answers: [{
+                text: 'Strong Telescope',
+                correct: false
+            },
+            {
+                text: 'Space Telescope',
+                correct: false
+            }, {
+                text: 'Hubble Space Telescope',
+                correct: true
+            }, {
+                text: 'Astro Telescope',
+                correct: false
+            }
+        ]
+    },
+    {
+        question: "Earth is located in which galaxy?",
+        answers: [{
+                text: 'The Mars Galaxy',
+                correct: false
+            },
+            {
+                text: 'The Milky Way Galaxy',
+                correct: true
+            }, {
+                text: 'The Snickers Galaxy',
+                correct: false
+            }, {
+                text: 'The Stars Galaxy',
+                correct: false
+            }
+        ]
+    },
+    {
+        question: 'Is the sun a star or a planet?',
+        answers: [{
+                text: 'Star',
+                correct: true
+            },
+            {
+                text: 'Planet',
+                correct: false
+            }
+        ]
+    },
+    {
+        question: 'Have humans ever walked on Mars?',
+        answers: [{
+                text: 'Yes',
+                correct: false
+            },
+            {
+                text: 'No',
+                correct: true
+            }
+        ]
+    },
+    {
+        question: 'Is the planet Neptune bigger than Earth?',
+        answers: [{
+                text: 'Yes ',
+                correct: true
+            },
+            {
+                text: 'No',
+                correct: false
+            }
+
+        ]
+    },
+    {
+        question: 'Does the sun orbit the Earth?',
+        answers: [{
+                text: 'Yes',
+                correct: false
+            },
+            {
+                text: 'No',
+                correct: true
+
+            }
+        ]
+    },
+    {
+        question: 'Can humans breathe normally in space?',
+        answers: [{
+                text: 'Yes',
+                correct: false
+            },
+            {
+                text: 'No',
+                correct: true
+
+            }
+        ]
+    }
+
+]
+
 // Quiz code - Credit Web Dev Simplified - base code logic amended from his tutorial. Code has been modified and extra features/components added to it.
 
 //Declared constant variables
