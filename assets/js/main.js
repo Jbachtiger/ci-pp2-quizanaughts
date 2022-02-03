af98d836ba791ff3bc69e9324f63f40acdcde531
Jbachtiger <jbachtiger@gmail.com>
2022-02-03 12:07:13 +0000

    Add code improvements and tidy up naming conventions

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,8 +8,8 @@ const answerButtons = document.getElementById('answer-buttons');
 const questionCounterText = document.getElementById('question-counter');
 const totalCorrectAnswersText = document.getElementById('score-tally');
 const maxQuestions = 15;
-const InnerProgressBar = document.getElementById("inner-progress-bar");
-const OuterProgressBar = document.getElementById("outer-progress-bar");
+const innerProgressBar = document.getElementById('inner-progress-bar');
+const outerProgressBar = document.getElementById('outer-progress-bar');
 const countDownTimer = document.getElementById('timer');
 const welcomeHeading = document.getElementById('welcome-heading');
 const welcomeIntro = document.getElementById('welcome-intro');
@@ -17,10 +17,13 @@ const welcomeIntro = document.getElementById('welcome-intro');
 // Declared let variables
 let shuffledQuestions;
 let currentQuestionIndex;
-let score = 0;
-let questionCounter = 0;
+let score;
+let questionCounter;
 let quizTimer;
 
+// Intro text for quiz
+welcomeHeading.innerText = "Are you ready space adventurers?";
+welcomeIntro.innerText = "It's time for you to devle deep into space and put your knowledge to the test. Good luck!";
 
 // Click event for next button to increment to next question
 nextButton.addEventListener('click', () => {
@@ -28,15 +31,13 @@ nextButton.addEventListener('click', () => {
     nextQuestion();
 })
 
-// Welcome text for quiz menu
-welcomeHeading.innerText = "Are you ready space adventurers?";
-welcomeIntro.innerText = "It's time for you to devle deep into space and put your knowledge to the test. Good luck!";
-
 // Starts game
 function startGame() {
     startButton.classList.add('hide');
     shuffledQuestions = questions.sort(() => Math.random() - .5);
     currentQuestionIndex = 0;
+    score = 0;
+    questionCounter = 0;
     openModalButton.classList.add('hide');
     contactUsButton.classList.add('hide');
     homeButton.classList.add('hide');
@@ -46,28 +47,28 @@ function startGame() {
     quizQuestions.classList.remove('hide');
     questionCounterText.classList.remove('hide');
     totalCorrectAnswersText.classList.remove('hide');
-    OuterProgressBar.classList.remove('hide');
+    outerProgressBar.classList.remove('hide');
     countDownTimer.classList.remove('hide');
     nextQuestion();
-    timer();
+    initTimer();
 }
 
 // Get's question and shuffles it. Also adds question counter and score text as well as increments the question.
 function nextQuestion() {
-    resetQuiz();
+    refreshUIForNextQuestion();
     showQuestion(shuffledQuestions[currentQuestionIndex]);
     questionCounter++;
-    questionCounterText.innerText = `Question: ${questionCounter}/${maxQuestions}`;
+    questionCounterText.innerText = `Question: ${currentQuestionIndex + 1}/${maxQuestions}`;
     totalCorrectAnswersText.innerText = `Score ${score}`;
     // Updated progress bar
-    InnerProgressBar.style.width = `${(questionCounter / maxQuestions) * 100}%`;
+    innerProgressBar.style.width = `${(questionCounter / maxQuestions) * 100}%`;
 }
 
 
 // Countdown timer
-function timer() {
+function initTimer() {
     let timeleft = 60;
-    quizTimer = setInterval(function() {
+    quizTimerRef = setInterval(function() {
         timeleft--;
         document.getElementById('timer').textContent = `Time: ${timeleft} sec`;
         if (timeleft <= 0) {
@@ -75,9 +76,8 @@ function timer() {
             startButton.innerText = 'Play Again';
             startButton.addEventListener('click', startButton)
             startButton.classList.remove('hide');
-            score = 0, questionCounter = 0;
             nextButton.classList.add('hide');
-            clearInterval(quizTimer);
+            clearInterval(quizTimerRef);
             const btns = document.querySelectorAll("#answer-buttons .btn");
             btns.forEach(btn => btn.style.pointerEvents = 'none');
         }
@@ -85,7 +85,8 @@ function timer() {
 }
 
 // Shows questions and answers in quiz
-function showQuestion(question) {
+function showQuestion() {
+    const question = shuffledQuestions[currentQuestionIndex];
     questionElement.innerText = question.question;
     question.answers.forEach(answer => {
         const button = document.createElement('button');
@@ -94,14 +95,14 @@ function showQuestion(question) {
         if (answer.correct) {
             button.dataset.correct = answer.correct;
         }
-        button.addEventListener('click', selectAnswer)
+        button.addEventListener('click', onAnswerClick)
         answerButtons.appendChild(button)
     })
 
 }
 
 // Resets questions and answers to default state each time a new question is set
-function resetQuiz() {
+function refreshUIForNextQuestion() {
     nextButton.classList.add('hide');
     while (answerButtons.firstChild) {
         answerButtons.removeChild(answerButtons.firstChild);
@@ -109,16 +110,22 @@ function resetQuiz() {
     }
 }
 
+// Once an answer has been selected remove click ability from remaining answer buttons
+
+function removeClickListenersFromButtons(buttons) {
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].removeEventListener("click", onAnswerClick);
+    }
+}
+
 /*
 Checks to see if player has selected correct or wrong answer. Changes button colour depeding on the result.
 */
-function selectAnswer(event) {
+function onAnswerClick(event) {
     const selectedButton = event.target;
     const correct = selectedButton.dataset.correct;
     let answerButton = document.getElementsByClassName('btn');
-    for (let i = 0; i < answerButton.length; i++) {
-        answerButton[i].removeEventListener('click', selectAnswer);
-    }
+    removeClickListenersFromButtons(answerButton);
     if (correct) {
         selectedButton.style.backgroundColor = "green";
         score++;
@@ -131,9 +138,8 @@ function selectAnswer(event) {
     } else {
         startButton.innerText = 'Play Again';
         startButton.addEventListener('click', startButton)
-        score = 0, questionCounter = 0;
         startButton.classList.remove('hide');
         homeButton.classList.remove('hide');
-        clearInterval(quizTimer);
+        clearInterval(quizTimerRef);
     }
 }
